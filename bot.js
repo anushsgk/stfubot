32a61446906f7f3f467f78d7ab43a54897d1bc8a
anushsgk <34090407+anushsgk@users.noreply.github.com>
2020-07-12 19:41:15 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,29 @@
-app.listen(process.env.PORT);
-let port = process.env.PORT;
-if (port == null || port == "") {
-    port = 8000;
-}
-app.listen(port);
-
 const Discord = require('discord.js');
-const {
-    prefix,
-    token
-} = require('./config.json');
+
 const client = new Discord.Client();
 
-client.once('ready', () => {
-    console.log('Ready!');
-})
+ 
 
-client.on('message', message => {
-    if (message.content === '${prefix}ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
-    } else if (message.author.id === '365530592017776650') {
-        message.channel.send('u have smol pp');
-    }
+client.on('ready', () => {
+
+    console.log('I am ready!');
 
 });
 
-client.login(token);
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
