9403e389f5f10a9e2d832cfcda5248b2cd130b19
anushsgk <34090407+anushsgk@users.noreply.github.com>
2020-07-12 20:32:45 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix } = require(./config.json);
+const { prefix } = require(config.json);
 
 client.on('ready', () => {
     console.log('I am ready!');
