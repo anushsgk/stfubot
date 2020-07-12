bcfa2a28e904ff76edb7a1c049baafe352195b1c
anushsgk <34090407+anushsgk@users.noreply.github.com>
2020-07-12 20:25:22 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { prefix } = '$';
+const { prefix } = '%';
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === '${ prefix }ping') {
+        message.reply('pong');
+    }
+    else if (message.content === 'ping') {
         message.reply('pong');
     }
 });
