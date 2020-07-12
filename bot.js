1469129c8650d8b52742d4a56ea2481a613543b4
anushsgk <34090407+anushsgk@users.noreply.github.com>
2020-07-12 20:15:03 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
-const {
-    prefix,
-    token
-} = require('./config.json');
-const client = new Discord.Client();
-
-client.once('ready', () => {
-    console.log('Ready!');
-})
 client.on('message', message => {
-    if (message.content === '${prefix}ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
-    } else if (message.author.id === '365530592017776650') {
-        message.channel.send('u have smol pp');
+    if (message.content === 'ping') {
+        message.reply('pong');
     }
-}
 });
 
 // THIS  MUST  BE  THIS  WAY
