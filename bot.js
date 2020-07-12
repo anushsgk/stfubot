ac5a4bcea09d9d295ea42629c302a3e591650e0f
anushsgk <34090407+anushsgk@users.noreply.github.com>
2020-07-12 20:13:05 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
 
+const {
+    prefix,
+    token
+} = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+})
 client.on('message', message => {
     if (message.content === '${prefix}ping') {
         // send back "Pong." to the channel the message was sent in
