5b503c2bc8d9d946b2b7ba58dd9639d641807af1
anushsgk <34090407+anushsgk@users.noreply.github.com>
2020-07-13 10:23:36 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,38 +1,38 @@
-//const Discord = require('discord.js');
-//const client = new Discord.Client();
-//
-//client.on('ready', () => {
-//    console.log('I am ready!');
-//});
-//
-//client.on('message', message => {
-//    if (message.content === 'ping') {
-//        message.reply('pong');
-//    }
-//});
-//
-//// THIS  MUST  BE  THIS  WAY
-//
-//client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {config, prefix} = require('./config.json');
 
 client.on('ready', () => {
-    console.log('I am ready!');
+   console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === `${prefix}ping`) {
-        message.reply('pong');
-    }
-    else if (message.content === 'ping') {
-        message.reply('long');
-    }
+   if (message.content === 'ping') {
+       message.reply('pong');
+   }
 });
 
 // THIS  MUST  BE  THIS  WAY
+
 client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
+
+
+// const Discord = require('discord.js');
+// const client = new Discord.Client();
+// const {config, prefix} = require('./config.json');
+
+// client.on('ready', () => {
+//     console.log('I am ready!');
+// });
+
+// client.on('message', message => {
+//     if (message.content === `${prefix}ping`) {
+//         message.reply('pong');
+//     }
+//     else if (message.content === 'ping') {
+//         message.reply('long');
+//     }
+// });
+
+// // THIS  MUST  BE  THIS  WAY
+// client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
 
